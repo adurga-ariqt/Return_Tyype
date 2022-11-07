40907931c3ccabcd3ececaebd4cb7c819123c73c
adurga-ariqt <adurga@ariqt.com>
2022-11-07 18:06:17 +0530

    Add project files.

---
+++ Return_Tyype/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Return_Tyype;
+
+Console.WriteLine("Hello, World!");
+Programe1 p = new Programe1();
+
+string name = p.Show("Rahul Kumar");
+Console.WriteLine("Hello " + name);  
+      
---
+++ Return_Tyype/Programe1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Tyype
+{
+    public  class Programe1
+    {
+        public string Show(string message)
+        {
+            Console.WriteLine("Inside Show Function");
+            return message;
+        }
+    }
+}
